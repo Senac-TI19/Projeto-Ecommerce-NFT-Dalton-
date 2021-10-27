fc30c49ec895e6fcd6b5cf610e1a5b6aaeb536e1
daltonmendes <dalton.m_jc@hotmail.com>
2021-10-27 12:37:42 -0300

    alterações servicWorker

---
+++ serviceWorker.js
@@ -0,0 +1,29 @@
+const cachename = 'cache-app-v1'
+
+const recursosChacheados = [
+    "./index.css", "./favicon.ico", "./index.html",
+    "./logar.html", "./quemsomos.html", "./registrar.html",
+    "./index.js", "./", "./icons/apple-icon-57x57.png", "./icons/apple-icon-60x60.png",
+    "./icons/apple-icon-72x72.png", "./icons/apple-icon-76x76.png",
+    "./icons/apple-icon-114x114.png", "./icons/apple-icon-120x120.png",
+    "./icons/apple-icon-144x144.png", "./icons/apple-icon-152x152.png",
+    "./icons/apple-icon-180x180.png", "./icons/favicon-512x512.png",
+    "./icons/android-icon-192x192.png", "./icons/favicon-32x32.png",
+    "./icons/favicon-96x96.png", "./icons/favicon-16x16.png",
+    "./icons/ms-icon-144x144.png",
+];
+
+
+self.addEventListener("install", function(event) {
+    console.log("Service Worker instalado!");
+    event.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(recursosCacheados);
+        })
+    );
+});
+
+self.addEventListener("activate", function() {
+    console.log("Service worker ativado!")
+
+});
